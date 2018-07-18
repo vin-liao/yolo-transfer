f90dbb47b4b2b38a932a989f75e52f3414bfa994
Vincent Liao <vincent.liao@protonmail.com>
2018-07-18 12:06:28 +0700

    rename bottleneck.py

--- bottleneck_features.py
+++ bottleneck_features.py
@@ -1,8 +1,8 @@
 import getopt
 import sys
-from keras.models import load_model
 import yolo_utils
 import numpy as np
+from keras.models import load_model
 from keras.models import Model
 
 wider_path = './wider_dataset'
@@ -10,7 +10,7 @@ image_path = wider_path + '/WIDER_train/images'
 bbox_path = wider_path + '/wider_face_train_bbx_gt.txt'
 quantity = 100
 
-#create input from terminal
+#take parameter from terminal
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'q:', ['qty='])
 except getopt.GetoptError:
@@ -27,6 +27,6 @@ model = load_model('yolo.h5')
 
 #only take the first until the second last layer
 intermediate_model = Model(inputs=model.input, outputs=model.get_layer('leaky_re_lu_8').output)
-bottleneck_features = intermediate_model.predict_generator(data, steps=1)
+bottleneck_features = intermediate_model.predict(data[0])
 np.save(open('bottleneck_features.npy', 'wb'), bottleneck_features)
-print('Done saving bottleneck features.')
+print('Bottleneck features saved.')
